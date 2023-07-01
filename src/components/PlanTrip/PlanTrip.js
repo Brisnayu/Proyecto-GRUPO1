93e74a91554125b6b20bb23420e5c11c67d076f2
RadiooGaga <mariahkatt@gmail.com>
2023-07-01 14:16:44 +0200

    terminando plantrip

--- src/components/PlanTrip/PlanTrip.js
+++ src/components/PlanTrip/PlanTrip.js
@@ -2,36 +2,43 @@ import "./PlanTrip.css";
 
 export const PlanTrip = () => {
   return `
-    <article>
-        <section>
-            <img src="./src/assets/img/img1.png" alt="img1" />
-            <img src="./src/assets/img/img2.png" alt="img2" />
-            <img src="./src/assets/img/img3.png" alt="img3" />
+    <article class="plantrip-container">
+
+        <section class="plantrip-img">
+            <img src="./src/assets/img/tresfotos.png" alt="tresfotos"> 
+            
         </section>
-        <section>
-            <div>
-                <h4>About</h4>
+
+        <section class="plantrip-info">
+
+            <div class="plantrip-main">
                 <h2>Plan your perfect trip</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eligendi eos repellat, qui sapiente a? Odio impedit explicabo, ut voluptate eligendi voluptatem incidunt praesentium at. Et deserunt exercitationem aut repellat?</p>
             </div>
-            <div>
+
+            <div class="plantrip-statistics">
             <div>
                 <h3>150+</h3>
                 <p>Flight Destinations</p>
             </div>
+
             <div>
                 <h3>250+</h3>
                 <p>Hotels</p>
             </div>
+
             <div>
                 <h3>80</h3>
                 <p>Elite Transportation</p>
             </div>
+
             <div>
                 <h3>40+</h3>
                 <p>We help to find your dream place</p>
             </div>
+
             </div>
+
         </section>
     </article>
     `;
