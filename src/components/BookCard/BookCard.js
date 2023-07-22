7dab675c96caf6219d59ab8d8e16074fafee534c
willCodeForCandy <lavirgin@gmail.com>
2023-07-22 13:06:41 +0200

    maquetado de tarjetas de hoteles y destinos

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -4,14 +4,15 @@ import "./BookCard.css";
 export const BookCard = (elemento) => {
   const {img, city, description, price, type} = elemento
     return `
-      <div class="container-book">
-          <img src=${img} alt=${city} />
+      <div class="container-book" style="background-image:url(${img})">
           <div class="container-text-book">   
+            <div>
               <h2>${city}</h2>
               <p>${description}</p>
+            </div>
               <h2>$ ${price}</h2>
           </div>
-          <button>Book ${type}</button>
+          <button class="booking-btn">Book ${type}</button>
       </div>
   
     `;
