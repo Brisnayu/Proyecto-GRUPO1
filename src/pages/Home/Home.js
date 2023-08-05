f3b63db0119f85b6a5f3d91189283c512c00dc0d
willCodeForCandy <lavirgin@gmail.com>
2023-08-05 12:27:41 +0200

    create category cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import "./Home.css";
 import PlanTrip from "../../components/PlanTrip/PlanTrip";
 import { Citycard } from "../../components/CityCard/citycard";
 import { fetchDestinations } from "../../utils/destinationsFetch";
+import { CategoryCard } from "../../components/CategoryCard/CategoryCard";
+
 
 export const Home = () => {
     const main = document.querySelector("main");
@@ -19,6 +21,10 @@ export const Home = () => {
             ${Citycard("./src/assets/img/phuket.png", "Paris", "Francia")}
             ${Citycard("./src/assets/img/phuket.png", "Phuket", "Thailand")}
             </div>
+        </div>
+        <div class="category-links">
+        ${CategoryCard("./src/assets/img/flights-category-image.png", "Flights", "Search flights & places and book for your amazing trips.")}
+        ${CategoryCard("./src/assets/img/hotels-category-image.png", "Hotels", "Search our best hotels & places and book for your  amazing trips.")}
         </div>
     `
 };
